8419e2b72d7b28f2e250a5fa9d19307838b29e66
nikhilrana07 <kumarnikil220@gmail.com>
2026-06-17 18:09:45 +0530

    project setup

---
+++ src/main/java/com/project/trendora/TrendoraApplication.java
@@ -0,0 +1,13 @@
+package com.project.trendora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendoraApplication.class, args);
+	}
+
+}
